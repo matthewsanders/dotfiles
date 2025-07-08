c3e666686320c7cbfb00e7c79031d879314dc5ef
Matthew Sanders <matthew1061@gmail.com>
2025-07-08 10:41:20 -0500

    fixed syntax error

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,4 +1,4 @@
-{
+return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
   branch = 'master',
@@ -6,31 +6,30 @@
   config = function()
     require('nvim-treesitter.configs').setup {
       ensure_installed = { 
-	      	"lua", 
-      		"bash", 
-		"json", 
-		"python", 
-		"c", 
-		"javascript", 
-		"css", "csv", 
-		"xml", 
-		"yaml", 
-		"cmake", 
-		"cpp", 
-		"typescript",
-		"dockerfile",
-		"editorconfig",
-		"git_config",
-		"gitignore",
-		"go",
-		"html",
-		"markdown",
-		"rust",
-		"sql"
-	},
+        "lua", 
+        "bash", 
+        "json", 
+        "python", 
+        "c", 
+        "javascript", 
+        "css", "csv", 
+        "xml", 
+        "yaml", 
+        "cmake", 
+        "cpp", 
+        "typescript",
+        "dockerfile",
+        "editorconfig",
+        "git_config",
+        "gitignore",
+        "go",
+        "html",
+        "markdown",
+        "rust",
+        "sql"
+      },
       highlight = { enable = true },
       indent = { enable = true },
     }
   end
 }
-
